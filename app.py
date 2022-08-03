6d523babad80f50ea35a98b7a3b61d86475b05b3
david-delgrosso <daviddelgrosso97@gmail.com>
2022-08-03 16:46:10 -0400

    Added priority functionality - Reorder tasks after deletion - Add top priority button - Reorder tasks if top priority is selected

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import I
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,20 +12,28 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 
 @app.route("/")
 def home():
-    todo_list = Todo.query.all()
+    todo_list = Todo.query.order_by(Todo.number).all()
     return render_template("index.html", todo_list=todo_list)
 
 
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
+    check = request.form.get("priorityCheck")
+    n = Todo.query.count() + 1
+    if check is not None:
+        new_todo = Todo(title=title, number=1, complete=False)
+        for row in Todo.query:
+            row.number = row.number + 1
+    else:
+        new_todo = Todo(title=title, number=n, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
@@ -42,6 +51,10 @@ def update(todo_id):
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
+    i = 1
+    for row in Todo.query:
+        row.number = i
+        i += 1
     db.session.commit()
     return redirect(url_for("home"))
 
